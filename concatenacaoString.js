5322a1225a41cbb911b53cc75c27a76de0c5e2c8
Emerson <the.emerson.araujo@gmail.com>
2025-02-07 14:11:53 -0300

    add console log for string concatenation example

--- concatenacaoString.js
+++ concatenacaoString.js
@@ -1,3 +1,4 @@
 let linguagem = "JavaScript";
 // console.log('Estou aprendendo concatenação de string em ' + linguagem + ' é maravilhoso!')
 // console.log(`Estou aprendendo concatenação de string em ${linguagem} é ótimo`)
+console.log("Estou aprendendo concatenação de string em", linguagem, "é maravavilhoso!")
